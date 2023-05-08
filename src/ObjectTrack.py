seems to have fixed rotation

src/ObjectTrack.py
@@ -66,7 +66,7 @@ class ObjectTrack:
     #   self.theta = -np.pi + self.theta
     # print(f"prediction_theta {self.theta}",end="\t")
     new_posn = mfn.pol2car((lx,ly), self.v[-1], self.theta)
-    print(f"predicted bbox: {new_posn}")
+    # print(f"predicted bbox: {new_posn}")
     return new_posn
     # return (lx + (self.r * np.cos(self.theta)), ly + (self.r * np.sin(self.theta)))
   

src/SA-test.py
@@ -37,7 +37,14 @@ OFFT = 20
 SPLINE_COUNT = 2
 TRANSLATE = False
 
-
+def adjust_angle(theta):
+  ''' adjusts some theta to arctan2 interval [0,pi] and [-pi, 0]'''
+  if theta > np.pi:
+    theta = theta + -2 * np.pi
+  elif theta < -np.pi:
+    theta = theta + 2 * np.pi
+  
+  return theta
 def draw_coordinate_frame(screen, sensor):
   '''
   Helper function for displaying the sensor field of view
@@ -109,14 +116,14 @@ def repeatable_environment_test(screen, sensing_agent, environment):
 
   vert_destinations.reverse()
   # horiz_destinations.reverse()
-  for i in reversed(horiz_destinations):
+  for i in reversed(vert_destinations):#horiz_destinations):
     vert_destinations.append(i)
     horiz_destinations.append(i)
   
   
   ptr = environment.targets[0].get_origin()
   pafn.frame_draw_dot(screen, ptr, pafn.colors["green"])
-  translation_path = horiz_destinations
+  translation_path = vert_destinations
   # translation_path
   while 1:
     for event in pygame.event.get():
@@ -128,8 +135,11 @@ def repeatable_environment_test(screen, sensing_agent, environment):
           for pt in translation_path[1:]:
             pafn.clear_frame(screen)
             # sensing_agent.predict()
+            # print(sensing_agent.estimate_next_rotation())
             curr_pt, pred_pt = sensing_agent.estimate_next_detection()
+            sensing_agent.estimate_next_rotation()
             if len(pred_pt):
+              
               # print((curr_pt,pred_pt))
               pafn.frame_draw_dot(screen, curr_pt, pafn.colors["red"])
               pafn.frame_draw_dot(screen, pred_pt, pafn.colors["yellow"])
@@ -168,13 +178,139 @@ def repeatable_environment_test(screen, sensing_agent, environment):
           while pygame.MOUSEBUTTONUP not in [event.type for event in pygame.event.get()]:
             continue
           p = pygame.mouse.get_pos()
+          # pts.append(p)
+          # dc = sensing_agent.transform_to_local_bbox(p)
           pafn.clear_frame(screen)
+          orig_theta = sensing_agent.get_fov_theta()
           rotation = sensing_agent.rotate_agent(p)
+          
+          new_theta = sensing_agent.get_fov_theta()
+          print(new_theta - orig_theta)
+          dc = sensing_agent.transform_to_local_bbox(p)
+          # print(f"displacement: {rotation / sensing_agent.get_fov_width() * 100}")
+          # dc = sensing_agent.transform_to_local_bbox(p)
+          print(f"original {p}\ndc {dc}")
+          print(new_theta)
+          
+          # sensing_agent.obj_tracker.add_angular_displacement(0, -(new_theta - orig_theta))
+          
           draw_sensing_agent(screen, sensing_agent)
           pygame.display.update()
           continue
          
+def repeatable_step_test(screen, sensing_agent, environment):
+  directions = [-np.pi, -np.pi / 2, 0,  np.pi / 2]
+  target_points = [(450,450), (550, 450), (550,550), (450,550)]
+  draw_sensing_agent(screen, environment.agent)
+  pygame.display.update()
+  
+  step_size = 25
+  vert_destinations = []
+  horiz_destinations = []
+  origin = (600,500)
+  for i in range(25):
+    x,y = origin
+    # destinations.append((x, y - step_size * i))
+    vert_destinations.append((x, y - step_size * i))
+    horiz_destinations.append((x - step_size * i, y))
+  
+  vert_destinations.reverse()
+  # horiz_destinations.reverse()
+  for i in reversed(vert_destinations):#horiz_destinations):
+    vert_destinations.append(i)
+    horiz_destinations.append(i)
+  
+  while 1:
+    for event in pygame.event.get():
+      if event.type == pygame.MOUSEBUTTONDOWN:
+        if pygame.key.get_mods() == SPACE:
+          continue
+        elif pygame.key.get_mods() == LSHIFT:  # rotate relative
+          continue
+        elif pygame.key.get_mods() == LALT: # estimate
+            curr_pt, pred_pt = sensing_agent.estimate_next_detection()
 
+            if len(pred_pt):
+              # print((curr_pt,pred_pt))
+              pafn.frame_draw_dot(screen, curr_pt, pafn.colors["red"])
+              pafn.frame_draw_dot(screen, pred_pt, pafn.colors["yellow"])
+              pafn.frame_draw_line(screen, (curr_pt, pred_pt),pafn.colors["white"])
+            pygame.display.update()
+            continue
+        elif pygame.key.get_mods() == LCTRL:
+          while pygame.MOUSEBUTTONUP not in [event.type for event in pygame.event.get()]:
+            continue
+          p = pygame.mouse.get_pos()
+          pafn.clear_frame(screen)
+          orig = sensing_agent.get_fov_theta()
+          rotation = sensing_agent.rotate_agent(p)
+          new_theta = sensing_agent.get_fov_theta()
+          print("checking...")
+          print(new_theta - orig)
+          print(rotation)
+          # sensing_agent.obj_tracker.add_angular_displacement(0,orig - new_theta)
+          draw_sensing_agent(screen, sensing_agent)
+          pygame.display.update()
+          continue
+          # pafn.frame_draw_dot(screen, pt, pafn.colors["green"])
+        else:
+          while pygame.MOUSEBUTTONUP not in [event.type for event in pygame.event.get()]:
+            continue
+          p = pygame.mouse.get_pos()
+          translation_path = []
+          # translation_path = gfn.lerp_list(environment.targets[0].get_origin(), p, 10)
+          translation_path = vert_destinations
+          for pt in translation_path[1:]:
+            pafn.clear_frame(screen)
+            # curr_pt, pred_pt = sensing_agent.estimate_next_detection()
+            # specify displacement
+            est_rotation = ()
+            est_rotation = sensing_agent.estimate_next_rotation()
+            
+            pred_rotation = sensing_agent.exoskeleton.get_relative_rotation(pt)
+            if len(est_rotation):
+              
+              est_rotation = est_rotation[0]
+              print(est_rotation)
+              rotation = sensing_agent.apply_rotation_to_agent(est_rotation)
+              sensing_agent.obj_tracker.add_angular_displacement(0, -est_rotation)
+              sensing_agent.exoskeleton.rel_theta += rotation
+              # print(f"sensing: {sensing_agent.exoskeleton.rel_theta}")
+              # sensing_agent.obj_tracker.add_angular_displacement(0, -est_rotation)
+              # draw_sensing_agent(screen, sensing_agent)
+              # pygame.display.update()
+              time.sleep(0.01)
+
+              # rotation = sensing_agent.apply_rotation_to_agent(-est_rotation)
+              # sensing_agent.exoskeleton.rel_theta += rotation
+              # sensing_agent.obj_tracker.add_angular_displacement(0, est_rotation)
+              # draw_sensing_agent(screen, sensing_agent)
+              # pygame.display.update()
+              # time.sleep(0.2)
+            # sensing_agent.obj_tracker.add_angular_displacement(0, -rotation)
+
+
+            # print(f"est:\t{est_rotation}\npred:\t{pred_rotation}")
+            curr_pt, pred_pt = sensing_agent.estimate_next_detection()
+            if len(pred_pt):
+              # print((curr_pt,pred_pt))
+              pafn.frame_draw_dot(screen, curr_pt, pafn.colors["red"])
+              pafn.frame_draw_dot(screen, pred_pt, pafn.colors["tangerine"])
+              pafn.frame_draw_line(screen, (curr_pt, pred_pt),pafn.colors["white"])
+            # npt = sensing_agent.estimate_next_rotation()
+            # if len(npt):
+            #   npt = sensing_agent.transform_from_local_coord(npt[0], npt[1])
+            #   pafn.frame_draw_dot(screen, npt, pafn.colors["cyan"])
+            
+
+            draw_sensing_agent(screen, sensing_agent)
+            pafn.frame_draw_dot(screen, pt, pafn.colors["green"])
+            environment.targets[0].origin = pt
+            environment.visible_targets()
+            pygame.display.update()
+            time.sleep(0.1)
+            # sensing_agentestimate_next_rotation()
+          continue
 
 def repeatable_sensing_agent(screen, sensing_agent):
   draw_sensing_agent(screen, sensing_agent)
@@ -248,10 +384,12 @@ def main():
   sensing_agent.exoskeleton = rb
   sensing_agent.sensor = sensor
   sensing_agent.obj_tracker = ObjectTrackManager()
+  sensing_agent.obj_tracker.parent_agent = sensing_agent
   target = Target((500,550))
   environment = Environment(sensing_agent, [target])
   # repeatable_sensing_agent(screen, sensing_agent)
-  repeatable_environment_test(screen, sensing_agent, environment)
+  # repeatable_environment_test(screen, sensing_agent, environment)
+  repeatable_step_test(screen, sensing_agent, environment)
 
 
 

src/SensingAgent.py
@@ -148,8 +148,6 @@ class SensingAgent:
       return False
     return True
 
-
-
   def is_rel_detectable(self, target_pt):
     '''
     Indicates whether a target point is detectable (within tolerance)
@@ -212,34 +210,9 @@ class SensingAgent:
     y = r
     w = 1
     h = 1
-    print(f"post_transform: {ratio}, {(x,y)}")
-    return [x,y,w,h]
-  
-  def _transform_to_local_bbox(self, target):
-    '''
-    Calculates detection coordinates relative to Agent
-    returns a Yolo Formatted bbox
-    '''
-    tar_theta, tar_r = mfn.car2pol(self.get_center(), target)
-  
-    org_theta = self.get_fov_theta()
-    if org_theta < 0:
-      org_theta = 2 * np.pi + org_theta
-
-    lh = org_theta + self.get_fov_width() / 2
-    rh = org_theta - self.get_fov_width() / 2
-    if tar_theta < rh:
-      tar_theta = tar_theta + 2 * np.pi
-    
-    ratio = (lh - tar_theta) / (lh - rh)
-    
-    x = Sensor.WINDOW_WIDTH * ratio
-    y = tar_r
-    w = 1
-    h = 1
+    # print(f"post_transform: {ratio}, {(x,y)}")
     return [x,y,w,h]
 
-  
   def transform_from_local_coord(self, x, y, w=1, h=1):
     '''
     Transforms a bbox from sensor local coords to world coords
@@ -252,6 +225,24 @@ class SensingAgent:
 
     return pt
 
+  def estimate_next_rotation(self, idx = 0):
+    curr_pt, pred_pt = self.estimate_rel_next_detection()
+    if not len(pred_pt):
+      return ()
+    if curr_pt == pred_pt:
+      return ()
+    status, flag = self.is_rel_detectable(pred_pt)
+    if status:
+      return ()
+    print("rotation necessary")
+    partial_rotation = (pred_pt[0] - 50) / 100 * self.get_fov_width()
+    return (partial_rotation,())
+    
+    
+
+    # rotation = abs(pred_theta - curr_theta)
+    # print(status)
+  
   def estimate_rel_next_detection(self, idx = 0):
     '''
     Estimates next detection in local coordinate system

src/Sensor.py
@@ -24,7 +24,7 @@ class Sensor:
   def __init__( self,
                 parent_agent,
                 sensor_radius = 300,
-                sensor_width = np.pi / 4):
+                sensor_width = np.pi / 3):
     self.parent_agent = parent_agent
     self.fov_radius = sensor_radius
     self.fov_width = sensor_width

src/StreamingObjectTrackManager.py
@@ -14,6 +14,16 @@ from categories import CATEGORIES
   global_track_store: {track_id : ObjectTrack}
       lookup dictionary for directly accessing track objects by ID
 '''
+
+def adjust_angle(theta):
+  ''' adjusts some theta to arctan2 interval [0,pi] and [-pi, 0]'''
+  if theta > np.pi:
+    theta = theta + -2 * np.pi
+  elif theta < -np.pi:
+    theta = theta + 2 * np.pi
+  
+  return theta
+
 LABELS = True
 IDENTIFIERS = not LABELS
 BOXES = IDENTIFIERS
@@ -69,6 +79,29 @@ class ObjectTrackManager:
       estimates.append(trk.predict_next_box())
     return estimates
 
+  def add_angular_displacement(self, distance, angle):
+    if not self.has_active_tracks():
+      return
+    
+    print(self.parent_agent.get_fov_theta())
+    
+    for i in range(len(self.active_tracks)):
+      trk = self.active_tracks[i]
+      last_d, last_v, delta_v, theta = trk.get_track_heading()
+
+      disp = angle / self.parent_agent.get_fov_width() * 100
+      print(disp)
+      # disp = [-50,50]
+      new_posn = [last_d[0] + disp, last_d[1]]
+      print(f"trk_theta {trk.theta + angle}")
+      trk.theta = adjust_angle(trk.theta - angle)
+      # new_posn = mfn.pol2car((50,0), last_d[1], angle)
+      # bbox = 
+      trk.path[-1].bbox = [new_posn[0],new_posn[1], 1,1]
+      
+
+
+
   def add_linear_displacement(self, distance, angle):
     if not self.has_active_tracks():
       return
@@ -76,8 +109,8 @@ class ObjectTrackManager:
     for i in range(len(self.active_tracks)):
       trk = self.active_tracks[i]
     
-      bbox = trk.get_last_detection()
-      new_posn = mfn.pol2car((bbox[0],bbox[1]), distance, angle)
+      last_d, last_v, delta_v, theta = trk.get_track_heading()
+      new_posn = mfn.pol2car((last_d[0],last_d[1]), last_d[1], angle)
       # trk.bbox = 
       trk.path[-1].bbox = [new_posn[0],new_posn[1],1,1]
 