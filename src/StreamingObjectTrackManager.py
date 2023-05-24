attempted fix for rotation

src/StreamingObjectTrackManager.py
@@ -37,9 +37,9 @@ BOXES = IDENTIFIERS
 class ObjectTrackManager:
     constants = {
         "avg_tolerance": 10,
-        "track_lifespan": 5,
+        "track_lifespan": 2,
         "default_avg_dist": 10,
-        "radial_exclusion": 35,
+        "radial_exclusion": 50,
     }
     display_constants = {"trail_len": 0}
 
@@ -96,24 +96,36 @@ class ObjectTrackManager:
         """
         if not self.has_active_tracks():
             return
+        off_t = self.parent_agent.get_fov_width() / 2
 
         for i in range(len(self.active_tracks)):
             trk = self.active_tracks[i]
             last_d, last_v, delta_v, theta = trk.get_track_heading()
 
-            disp = angle / self.parent_agent.get_fov_width() * 100
-
+            ratio = angle / self.parent_agent.get_fov_width()
+            disp = ratio * 100
+            
+            # off_t = np.pi / 2 + angle
+            # off_t = disp / 100 * self.parent_agent.get_fov_width()
+            
+            orig_theta = adjust_angle(self.parent_agent.get_fov_theta() + angle)
+            
+            new_angle = 0
+            # # angle = 0
             if disp < 0:
-                trk.theta[-1] = adjust_angle(trk.theta[-1] + angle + np.pi / 2)
+                new_angle = adjust_angle(trk.theta[-1] + angle + off_t)
             if disp > 0:
                 if trk.theta[-1] < -np.pi / 2:
-                    trk.theta[-1] = adjust_angle(trk.theta[-1] - angle + np.pi / 2)
+                    new_angle = adjust_angle(trk.theta[-1] - angle + off_t)
                 else:
-                    trk.theta[-1] = adjust_angle(trk.theta[-1] + angle - np.pi / 2)
-
+                    new_angle = adjust_angle(trk.theta[-1] + angle - off_t)
+            print(f"orig:\t{orig_theta}\nangle:\t{angle}\nofft:\t{off_t}\ntrk:\t{trk.theta[-1]}\ndisp:\t{disp}\nnew:\t{new_angle}\n\n")
+            # print(f"trk.theta: {trk.theta[-1]}\tnew_angle: {new_angle}")
+            trk.theta[-1] = new_angle
             x, y = last_d
+            print(x)
             nx, ny = [last_d[0] + disp, last_d[1]]
-
+            # print(nx)
             trk.path[-1].bbox = [nx, ny, 1, 1]
 
     def add_linear_displacement(self, distance, angle):

src/test-driver.py
@@ -42,7 +42,8 @@ def agent_update(sensing_agent):
             print("rotation OOB")
         rotation = sensing_agent.apply_rotation_to_agent(est_rotation)
         direction = 1 if est_rotation > 0 else -1
-        print(f"sensing_agent theta: {sensing_agent.exoskeleton.rel_theta}")
+        print(f"original sensing_agent theta: {sensing_agent.exoskeleton.rel_theta}")
+        
         sensing_agent.exoskeleton.rel_theta += rotation
         if sensing_agent.exoskeleton.rel_theta < -np.pi:
             sensing_agent.exoskeleton.rel_theta = (
@@ -52,6 +53,7 @@ def agent_update(sensing_agent):
             sensing_agent.exoskeleton.rel_theta = (
                 -2 * np.pi + sensing_agent.exoskeleton.rel_theta
             )
+        
         sensing_agent.obj_tracker.add_angular_displacement(0, -est_rotation, direction)
     if est_translation != None:
         translation = sensing_agent.apply_translation_to_agent(est_translation)
@@ -156,6 +158,7 @@ def interactive_single_agent_test(screen, sensing_agent, environment):
                         )
                     pygame.display.update()
                     continue
+                
                 else:
                     while pygame.MOUSEBUTTONUP not in [
                         event.type for event in pygame.event.get()
@@ -190,7 +193,7 @@ def interactive_single_agent_test(screen, sensing_agent, environment):
                     pygame.display.update()
 
 def init_sensing_agent(
-    sensing_agent=SensingAgent(), origin=(0, 0), _id=0, orientation=(500, 500)
+    sensing_agent=SensingAgent(), origin=(0, 0), _id=0, orientation=(500, 400)
 ):
     ox, oy = origin
     scale = 2
@@ -212,7 +215,7 @@ def init_sensing_agent(
         rigid_link=ap,
     )
     sensor = Sensor(parent_agent=sensing_agent)
-    sensor.fov_width = 3 * np.pi / 5
+    sensor.fov_width =  3 * np.pi / 5
 
     sensing_agent.exoskeleton = rb
     sensing_agent.exoskeleton.states = []
@@ -249,7 +252,7 @@ def circular_test(screen, sensing_agent, environment):
         0.5235987755982988,
         0,
     ]
-    target_angles.reverse()
+    # target_angles.reverse()
     target_points = [mfn.pol2car((400, 400), 100, i) for i in target_angles]
     last_pt = None
     for pt in target_points:

src/test-multitrack.py
@@ -23,6 +23,33 @@ def adjust_angle(theta):
 
     return theta
 
+def agent_update(sensing_agent):
+    """
+    Updates the pose of a single agent
+    """
+    est_rotation, est_translation = sensing_agent.estimate_pose_update()
+
+    if est_rotation != None:
+        # if est_rotation > np.pi or est_rotation < -np.pi:
+        #     print("rotation OOB")
+        rotation = sensing_agent.apply_rotation_to_agent(est_rotation)
+        direction = 1 if est_rotation > 0 else -1
+        # print(f"sensing_agent theta: {sensing_agent.exoskeleton.rel_theta}")
+        sensing_agent.exoskeleton.rel_theta += rotation
+        if sensing_agent.exoskeleton.rel_theta < -np.pi:
+            sensing_agent.exoskeleton.rel_theta = (
+                2 * np.pi + sensing_agent.exoskeleton.rel_theta
+            )
+        if sensing_agent.exoskeleton.rel_theta > np.pi:
+            sensing_agent.exoskeleton.rel_theta = (
+                -2 * np.pi + sensing_agent.exoskeleton.rel_theta
+            )
+        sensing_agent.obj_tracker.add_angular_displacement(0, -est_rotation, direction)
+    if est_translation != None:
+        translation = sensing_agent.apply_translation_to_agent(est_translation)
+        sensing_agent.obj_tracker.add_linear_displacement(-translation, 0)
+
+
 def import_agent_record(screen, agent_record):
     """
     Imports a LOCO formatted json
@@ -84,25 +111,30 @@ def render_path(screen, path, color):
 def multitrack(screen, environment, paths,colors, n = 10):
   for k in environment.agents:
     draw_sensing_agent(screen, environment.agents[k])
-  for i in range(n):
+  for i in range(1,n):
+    pafn.clear_frame(screen)
+
+    for p in range(len(paths)):
+      render_path(screen, paths[p],colors[p])
+      
+    for k in environment.agents:
+      sensing_agent = environment.agents[k]
+      agent_update(sensing_agent)
+      draw_sensing_agent(screen, environment.agents[k])
+      # if sensing_agent.obj_tracker.active_tracks == None:
+        # continue
+      # for t in range(len(sensing_agent.obj_tracker.active_tracks)):
+      curr_pt, pred_pt = sensing_agent.estimate_next_detection()
+      if len(pred_pt):
+          # print((curr_pt,pred_pt))
+          pafn.frame_draw_dot(screen, curr_pt, pafn.colors["red"])
+          pafn.frame_draw_dot(screen, pred_pt, pafn.colors["yellow"])
+          pafn.frame_draw_line(
+              screen, (curr_pt, pred_pt), pafn.colors["white"]
+          )
     for j,t in enumerate(environment.targets):
       t.origin = paths[j][i]
       pafn.frame_draw_dot(screen, paths[j][i], colors[j])
-    
-    for k in environment.agents:
-      sensing_agent = environment.agents[k]
-      if sensing_agent.obj_tracker.active_tracks == None:
-        continue
-      for t in range(len(sensing_agent.obj_tracker.active_tracks)):
-        curr_pt, pred_pt = sensing_agent.estimate_next_detection(t)
-        if len(pred_pt):
-            # print((curr_pt,pred_pt))
-            pafn.frame_draw_dot(screen, curr_pt, pafn.colors["red"])
-            pafn.frame_draw_dot(screen, pred_pt, pafn.colors["yellow"])
-            pafn.frame_draw_line(
-                screen, (curr_pt, pred_pt), pafn.colors["white"]
-            )
-
     environment.visible_targets()
     pygame.display.update()
     time.sleep(0.2)
@@ -127,14 +159,14 @@ def path_handler(screen):
   min_x, min_y = 50,50
   max_x, max_y = 700,700
   # origins = [(100,50)]#, (50,50)]#, (50,100)]
-  origins = [(100,50), (50,100)]
-  origins.reverse()
+  origins = [(100,50)]#, (50,100)]
+  # origins.reverse()
   destinations = [(450,700), (700, 450), (700,700), (700,450)]
   vertices = []
   vertices.append(gfn.get_isosceles_vertex(origins[0], destinations[0],-1, 35))
   # vertices.append(gfn.get_isosceles_vertex(origins[1], destinations[1]))
   # vertices.append(gfn.get_midpoint(origins[1], destinations[1]))
-  vertices.append(gfn.get_isosceles_vertex(origins[1], destinations[1],1,35))
+  # vertices.append(gfn.get_isosceles_vertex(origins[1], destinations[1],1,35))
   n = 30
   paths = []
   for i in range(len(origins)):
@@ -150,12 +182,12 @@ def path_handler(screen):
   colors = [pafn.colors["green"], pafn.colors["red"], pafn.colors["cyan"]]
   environment = Environment()
   sensing_agent = init_sensing_agent()
-  sensing_agent.ALLOW_TRANSLATION = False
-  sensing_agent.ALLOW_ROTATION = False
+  # sensing_agent.ALLOW_TRANSLATION = False
+  # sensing_agent.ALLOW_ROTATION = False
   # theta, r = mfn.car2pol(origins[1], destinations[1])
-  sensing_agent.centered_sensor.fov_radius = 900
-  sensing_agent.centered_sensor.fov_width = np.pi / 2
-  sensing_agent.exoskeleton.fov_theta = np.pi / 4
+  sensing_agent.centered_sensor.fov_radius = 300
+  sensing_agent.centered_sensor.fov_width = np.pi / 4
+  # sensing_agent.exoskeleton.fov_theta = np.pi / 4
 
   for i in range(len(origins)):
     environment.add_target(Target(origins[i], _id=i))
@@ -194,7 +226,7 @@ def init_sensing_agent(
         rigid_link=ap,
     )
     sensor = Sensor(parent_agent=sensing_agent)
-    sensor.fov_width = 3 * np.pi / 5
+    sensor.fov_width = 1 * np.pi / 5
 
     sensing_agent.exoskeleton = rb
     sensing_agent.exoskeleton.states = []